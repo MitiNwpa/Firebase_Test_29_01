181702c5a181bc7710a0e9c97f4f696ae96809a0
HrishiGulhane <gulhane.hrishikesh@gmail.com>
2019-02-01 13:20:28 +1100

    trying navigate

--- src/ListDockets.js
+++ src/ListDockets.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import firebase from './firestore'
-
+import './index.css'
 const db = firebase.firestore();
 const refDoc = db.collection('promise');
 
@@ -11,12 +11,16 @@ class ListDockets extends Component {
     super();
     this.state = {
       items: []
+      // status: 'rejected'
     }
+    this.changeclass = this.changeclass.bind(this);
+    this.approveIt = this.approveIt.bind(this);
   }
 
   componentWillMount() {
     refDoc.onSnapshot((docSnapShot) => {
       let items = []
+
       docSnapShot.forEach(doc => { items.push(doc.data()) })
       this.setState({
         items,
@@ -24,6 +28,8 @@ class ListDockets extends Component {
       })
     })
   }
+
+
 
   deleteDocket = (e) => {
     refDoc.doc(e.target.value).delete().then(function () {
@@ -33,106 +39,106 @@ class ListDockets extends Component {
     });
   }
 
-    handleChange = (e) => {
-      const t = e.target
-      this.setState({
-        [t.name]: t.value
-      })
-    }
+  handleChange = (e) => {
+    const t = e.target
+    this.setState({
+      [t.name]: t.value
+    })
+  }
 
 
-    renderDockets() {
-      const ListItem = this.state.items.map((item, index) => {
-        return (
-          <li key={index}>
-            {item.email}
-            <br />
-            {item.company}
 
-            <button value={item.id} onClick={this.deleteDocket}>X</button>
-            <div>
-              approval status is {item.site}
-            </div>
-          </li>
-        )
-      })
-
-   
-
+  renderDockets() {
+    const ListItem = this.state.items.map((item, index) => {
       return (
-        <ul>
-          {ListItem}
-        </ul>
+        <li key={item.id} id={item.id} className={item.site}>
+          {item.email}
+          <br />
+          {item.company}
+
+          <button value={item.id} onClick={this.deleteDocket}>X</button>
+          <div>
+            approval status is {item.site}
+          </div>
+        </li>
       )
-    }
+    })
 
+    return (
+      <ul>
+        {ListItem}
+      </ul>
+    )
+  }
+  // where('site' , '==', 'rejected')
 
-    // renderDockets() {
-    //   const ListItem = this.state.items.map((item, index) => {
-        
-    //       (item.site == 'approved' ? 
-    //       return (
-    //       <ul>
-    //       <li key={index}>
-    //         {item.email}
-    //         <br />
-    //         {item.company}
+  changeclass() {
+    refDoc.onUpdate((docsnap, index) => {
+      docsnap.forEach(doc => {
+        console.log('hey')
+      });
+    })
+  }
 
-    //         <button value={item.id} onClick={this.deleteDocket}>X</button>
-    //         <div>
-    //           approval status is {item.site}
-    //         </div>
-    //       </li>
-    //       </ul>
-    //     ):return (
-    //       <ul id='complete'>
-    //       <li key={index}>
-    //         {item.email}
-    //         <br />
-    //         {item.company}
+  // renderDockets() {
+  //   const ListItem = this.state.items.map((item, index) => {
 
-    //         <button value={item.id} onClick={this.deleteDocket}>X</button>
-    //         <div>
-    //           approval status is {item.site}
-    //         </div>
-    //       </li> )
-    //   })
+  //       (item.site == 'approved' ? 
+  //       return (
+  //       <ul>
+  //       <li key={index}>
+  //         {item.email}
+  //         <br />
+  //         {item.company}
 
-   
+  //         <button value={item.id} onClick={this.deleteDocket}>X</button>
+  //         <div>
+  //           approval status is {item.site}
+  //         </div>
+  //       </li>
+  //       </ul>
+  //     ):return (
+  //       <ul id='complete'>
+  //       <li key={index}>
+  //         {item.email}
+  //         <br />
+  //         {item.company}
 
-    //   return (
-    //     <ul>
-    //       {ListItem}
-    //     </ul>
-    //   )
-    // }
-
-
-
-
-
-
-
-
-
+  //         <button value={item.id} onClick={this.deleteDocket}>X</button>
+  //         <div>
+  //           approval status is {item.site}
+  //         </div>
+  //       </li> )
+  //   })
 
 
 
+  //   return (
+  //     <ul>
+  //       {ListItem}
+  //     </ul>
+  //   )
+  // }
 
+  // approveIt() {
+  //   console.log('approved that ')
+  //   var stateCopy = Object.assign({}, this.state);
+  //   stateCopy.items[4].status = 'approved';
+  //   this.setState(stateCopy);
+  //   this.forceUpdate();
 
+  // }
 
-
-    
-    approvedDockets(){
-      var checked = refDoc.where('site', '==', 'approved')
-    }
 
 
   render() {
     return (
       <div>
         All Dockets here
-                {this.renderDockets()}
+        <button onClick={this.approveIt}>approve it</button>
+        <button>reject it</button>
+        {this.renderDockets()}
+
       </div>
     )
   }
